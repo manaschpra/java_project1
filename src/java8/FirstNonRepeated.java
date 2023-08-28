489af073b0c063f7a5066bffaf8260a4fc5388db
manaschpra <manaschpradhan06@gmail.com>
2023-08-28 13:20:10 +0530

    initial commit

---
+++ src/java8/FirstNonRepeated.java
@@ -0,0 +1,26 @@
+package java8;
+
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeated {
+
+	public static void main(String[] args) {
+
+		String input = "Java articles are Awesome";
+		Character result=input.chars()
+				.mapToObj(s->Character.toLowerCase(Character.valueOf((char)s)))
+				.collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new ,Collectors.counting()))
+				.entrySet()
+				.stream()
+				.filter(entry->entry.getValue() == 1L) //first non repeated character
+				//.filter(entry->entry.getValue() > 1L) //first repeated character
+				.map(entry->entry.getKey())
+				.findFirst()
+				.get();
+		System.out.println(result);
+		
+	}
+
+}
---
+++ src/java8/Hello.java
@@ -0,0 +1,15 @@
+package java8;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("jjjj");
+		String s1="hello";
+		System.out.println("s1 > " +s1);
+
+		String s2 =s1 + "world";
+		System.out.println("s2 > " +s2);
+
+	}
+
+}
